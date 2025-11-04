c30ea1b40c52a900b9885d9a4d7915efe62c81b6
OlEdKo <olgatsoy80@yandex.ru>
2025-11-04 13:24:29 +0000

    Removed intermediate assertion

--- configuration.py
+++ configuration.py
@@ -1,5 +1,5 @@
 # URL_SERVICE хранит базовый URL веб-сервиса, который используется для доступа к API
-URL_SERVICE = "https://9db3c8ed-b32c-4bd7-8ff1-92579f082ef3.serverhub.praktikum-services.ru"
+URL_SERVICE = "https://b36ae3e6-1c9c-444a-aaf5-75434904719f.serverhub.praktikum-services.ru"
 
 # Эндпоинт для создания заказа
 CREATE_ORDER_PATH = "/api/v1/orders"
--- create_order_test.py
+++ create_order_test.py
@@ -8,7 +8,6 @@ def test_get_order_by_track():
     
     # Вызов функции post_new_order с телом запроса для создания нового заказа из модуля data
     response = sender_stand_request.post_new_order(data.order_body)
-    assert response.status_code == 201, "Не удалось создать заказ"
 
     # Сохранение трэка заказа
     track_number = response.json()["track"]
